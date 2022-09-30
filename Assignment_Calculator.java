a4a4b939221fccb97a34c8c0605213f579e8963b
abelkristv <abel.kristanto@gmail.com>
2022-09-30 12:15:03 +0700

    refactor code into only 100 lines

--- Assignment_Calculator.java
+++ Assignment_Calculator.java
@@ -6,207 +6,90 @@ public class Assignment_Calculator{
 	public static double result = 0;
 
 	public static void clearConsole(){
-                try{
-                        if(System.getProperty("os.name").contains("Windows")){
-                                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-                        } else{
-                                System.out.print("\033\143");
-                        }
-                } catch (IOException | InterruptedException ex) {}
-        }
+		try{
+			if(System.getProperty("os.name").contains("Windows")){
+					new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+			} else{
+					System.out.print("\033\143");
+			}
+		} catch (IOException | InterruptedException ex) {}
+    }
 
-        public static void start(){
-        	double num_1 = 0, num_2 = 0;
+	public static double operation(int operator, double num){
+		switch(operator){
+			case 1:
+				return result += num;
+			case 2:
+				return result -= num;
+			case 3:
+				return result *= num;
+			case 4:
+				return result /= num;
+			case 5:
+				return result %= num;
+			case 6:
+				return 0;
+		}
+		return 0;
+	}
+
+    public static void start(){
+        double num_1 = 0, number = 0;
 		int operator = 0;
 
 		Scanner input = new Scanner(System.in);
-
 		do{
-			do{
-				if(result == 0){
-					clearConsole();
+			clearConsole();
+			result = operation(operator, number);
+			System.out.println("Calculator");
+			System.out.println("==================");
+			System.out.printf("Total = %.2f", result);
 
-                                	System.out.println("Calculator");
-                                	System.out.println("==================");
-                                	System.out.printf("Total = %.2f", result);
+			System.out.println("");
+			System.out.println("");
 
-                                	System.out.println("");
-                                	System.out.println("");
-
-                                	System.out.print("Input number : ");
-                                	try{
-                                		num_1 = input.nextDouble();
-                                	} catch(Exception e){
-                                		start();
-                                	}
-
-                                	System.out.println("1. Addition");
-                                	System.out.println("2. Subtraction");
-                                	System.out.println("3. Multiplication");
-                          		System.out.println("4. Division");
-                          	        System.out.println("5. Modulo");
-                  	                System.out.println("6. Clear Total");
-	                                System.out.println("7. Exit");
-     		                        System.out.print("Operator [1...7]: ");
-                                	try{
-                                		operator = input.nextInt();
-                                		if(operator < 1 || operator > 7){
-                                			start();
-                                		}
-                                	} catch(Exception e){
-                                		start();
-                                	}
-
-					if(operator == 7){
-                                	        System.exit(0);
-                                	}
-				} else{
-					clearConsole();
-
-                                        System.out.println("Calculator");
-                                        System.out.println("==================");
-                                        System.out.printf("Total = %.2f", result);
-
-                                        System.out.println("");
-                                        System.out.println("");
-
-                                        System.out.println("1. Addition");
-                                        System.out.println("2. Subtraction");
-                                        System.out.println("3. Multiplication");
-                                        System.out.println("4. Division");
-                                        System.out.println("5. Modulo");
-                                        System.out.println("6. Clear Total");
-                                        System.out.println("7. Exit");
-                                        System.out.print("Operator [1...7]: ");
-                                        try{
-                                		operator = input.nextInt();
-                                		if(operator < 1 || operator > 7){
-                                			start();
-                                		}
-                                	} catch(Exception e){
-                                		start();
-                                	}
-
-                                        if(operator == 7){
-                                                System.exit(0);
-                                        } else if(operator == 6){
-						break;
-					}
-				}
-			} while(operator < 1 || operator > 7);
-
-			if(result == 0){
-				switch(operator){
-					case 1:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-						result = num_1 + num_2;
-						break;
-					case 2:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-	                                        result = num_1 - num_2;
-	                                        break;
-					case 3:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-	                                        result = num_1 * num_2;
-	                                        break;
-					case 4:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-	                                        result = num_1 / num_2;
-	                                        break;
-					case 5:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-	                                        result = num_1 % num_2;
-	                                        break;
-					case 6:
-	                                        result = 0;
-	                                        break;
-					case 7:
-						System.exit(0);
-						break;
-				}
-			} else{
-				switch(operator){
-					case 1:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-						result = result + num_2;
-						break;
-					case 2:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-	                                        result = result - num_2;
-	                                        break;
-					case 3:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-	                                        result = result * num_2;
-	                                        break;
-					case 4:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-	                                        result = result / num_2;
-	                                        break;
-					case 5:
-						System.out.print("Input number : ");
-                        			try{
-	                                		num_2 = input.nextDouble();
-	                                	} catch(Exception e){
-	                                		start();
-	                                	}
-	                                        result = result % num_2;
-	                                        break;
-					case 6:
-	                                        result = 0;
-	                                        break;
-					case 7:
-						System.exit(0);
-						break;
+			
+			if (result == 0){
+				try{
+					System.out.print("Input number : ");
+					result = input.nextDouble();
+				} catch(Exception e){
+					continue;
 				}
 			}
-		} while(operator != 7);
-        }
+			System.out.println("1. Addition");
+			System.out.println("2. Subtraction");
+			System.out.println("3. Multiplication");
+			System.out.println("4. Division");
+			System.out.println("5. Modulo");
+			System.out.println("6. Clear Total");
+			System.out.println("7. Exit");
+			System.out.print("Operator [1...7]: ");
+			try{
+				operator = input.nextInt();
+				if(operator < 1 || operator > 7){
+					continue;
+				}
+				if (result > 0){
+					System.out.print("Input number : ");
+					try{
+						number = input.nextDouble();
+					} catch(Exception e){
+						continue;
+					}
+				}
 
+			} catch(Exception e){
+				continue;
+			}
+
+			if(operator == 7){
+				System.exit(0);
+			}
+		} while(true);
+
+	}
+	
 	public static void main(String[] args){
 		start();
 	}
